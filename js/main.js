ec15bc11392abea81a56bfb97bd17e8ca72d746b
Mohammed Ubaid Khan <56798024+mdubaidkhan@users.noreply.github.com>
2022-03-30 14:44:16 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+
+    document. getElementById("myButton"). onclick = function () {
+  location. href = "https://drive.google.com/file/d/1-0Q0RkFg4T5m3-0UoOnrzoF0DBENo9x7/view?usp=sharing";
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+      $('.project-area .grid .test-popup-link').magnificPopup({
+          type: 'image',
+          gallery: { enabled: true }
+      });
+  
+  
+      // Owl-carousel
+  
+      $('.site-main .about-area .owl-carousel').owlCarousel({
+          loop: true,
+          autoplay: true,
+          dots: true,
+          responsive: {
+              0: {
+                  items: 1
+              },
+              560: {
+                  items: 2
+              }
+          }
+      })
+  
+   var controller = new ScrollMagic.Controller();
+  
+    var ourScene = new ScrollMagic.Scene({
+      triggerElement:'#effect01',
+      duration:2000,
+      
+      })
+      .setClassToggle('#effect01','fade-in')
+      .addTo(controller);
+  
+      var ourScene1 = new ScrollMagic.Scene({
+          triggerElement:'#effect02',
+          duration:'70%',
+          
+          })
+          .setClassToggle('#effect02','fade-in1')
+         // .addIndicators()
+          .addTo(controller);
+  
+  
+      
+  
+  
+  
+  
+      
+  
+  
+  });
+  
+  
